622df6efbdb690f6cad7912ac57e5706caeaf43a
Emily Sombra <emmevsombra@gmail.com>
2024-10-29 20:46:23 -0300

    admin chat model

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Message
 
-# Register your models here.
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['content', 'sender', 'receiver', 'created_at']
+    ordering = ['created_at']
+
+
+admin.site.register(Message, MessageAdmin)
