6deb26de20aa9f152217b235ddf6a7fb45e48e3a
Emily Sombra <emmevsombra@gmail.com>
2024-10-29 23:18:52 -0300

    prep work

--- chat/views.py
+++ chat/views.py
@@ -1,12 +1,22 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins, status
+from rest_framework.response import Response
 from django_filters import rest_framework as filters
 from .models import Message
 from .serializers import MessageSerializer
 from .filters import MessageFilter
 
 
-class MessageViewSet(viewsets.ReadOnlyModelViewSet):
+class MessageViewSet(mixins.CreateModelMixin,
+                     mixins.ListModelMixin,
+                     viewsets.GenericViewSet):
     queryset = Message.objects.all()
     filter_backends = (filters.DjangoFilterBackend,)
     serializer_class = MessageSerializer
     filterset_class = MessageFilter
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
