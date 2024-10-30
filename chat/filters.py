5bb0ea2b8829d55ca4777ae7a9770838b9517574
Emily Sombra <emmevsombra@gmail.com>
2024-10-29 22:45:27 -0300

    fix filters

--- chat/filters.py
+++ chat/filters.py
@@ -1,8 +1,14 @@
 from django_filters import rest_framework as filters
+from django.db.models import Q
 from .models import Message
 
 
 class MessageFilter(filters.FilterSet):
+    user = filters.UUIDFilter(field_name='user', method='filter_user')
+
     class Meta:
         model = Message
         fields = ['receiver', 'sender']
+    
+    def filter_user(self, qs, name, value):
+        return qs.filter(Q(receiver=value) | Q(sender=value))
