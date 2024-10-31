16b9e836f4f50836a2dc8d95bd40e5d5ef6940ea
Emily Sombra <emmevsombra@gmail.com>
2024-10-30 23:29:04 -0300

    adding bot message

--- chat/serializers.py
+++ chat/serializers.py
@@ -6,3 +6,9 @@ class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = ('id', 'content', 'sender', 'created_at')
+
+
+class CreateMessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = ('content', 'receiver', 'sender')
--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets, mixins, status
 from rest_framework.response import Response
 from django_filters import rest_framework as filters
+from uuid import UUID
 from .models import Message
-from .serializers import MessageSerializer
+from .serializers import CreateMessageSerializer, MessageSerializer
 from .filters import MessageFilter
 
 
@@ -14,9 +15,26 @@ class MessageViewSet(mixins.CreateModelMixin,
     serializer_class = MessageSerializer
     filterset_class = MessageFilter
     
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return CreateMessageSerializer
+
+        return MessageSerializer
+
+    def new_bot_message(self):
+        # ('content', 'receiver', 'sender')
+        data = {'receiver': UUID('3352f124-ea31-4c99-b9d1-111d97e4d892'),
+                'sender': UUID('e7d81ea5-d89c-40b3-9cd3-3ed8fb6c53d5'),
+                'content': 'generated message'}
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return serializer
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        new_serializer = self.new_bot_message()
+        headers = self.get_success_headers(new_serializer.data)
+        return Response(new_serializer.data, status=status.HTTP_201_CREATED, headers=headers)
