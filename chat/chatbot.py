f3c77f2cdffcc841a35cf75ccf61fa275701cb1b
Emily Sombra <emmevsombra@gmail.com>
2025-01-17 22:49:09 -0300

    this time it will work, right???

--- chat/chatbot.py
+++ chat/chatbot.py
@@ -2,25 +2,26 @@ import openai
 from dotenv import load_dotenv
 import os
 
-base_messages = [
-    {'role': 'system', 'content': 'Você é um assistente prestativo, especializado em Engenharia de Software e Testes de Software.'}
-]
-
 
 class ChatBot:
     def __init__(self):
         pass
 
     def get_response(self, input):
+        messages = [
+            {'role': 'system',
+             'content': 'Você é um assistente prestativo, especializado em Engenharia de Software e Testes de Software.'},
+            {'role': 'user',
+             'content': input}
+        ]
         try:
-            load_dotenv()
             client = openai.OpenAI(
                 api_key = os.getenv('OPENAI_API_KEY'),
                 base_url = 'https://chat.maritaca.ai/api',
             )
             response = client.chat.completions.create(
                 model='sabia-3',
-                messages=base_messages.append({'role': 'user', 'content': input}),
+                messages=messages,
                 max_tokens=2000
             )
             return response.choices[0].message.content
